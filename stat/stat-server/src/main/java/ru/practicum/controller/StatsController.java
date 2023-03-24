25d9e067ec0bc1dce36ba995d4d36f4bff4b3e8c
baser <admhelperpc@gmail.com>
2023-03-24 11:15:35 +0300

    Правки клиента

--- stat/stat-server/src/main/java/ru/practicum/controller/StatsController.java
+++ stat/stat-server/src/main/java/ru/practicum/controller/StatsController.java
@@ -2,12 +2,7 @@ package ru.practicum.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.practicum.EndpointHitDto;
 import ru.practicum.ViewStatsDto;
 import ru.practicum.service.StatsService;
