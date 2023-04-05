2fa8adffcf849b8d259e9e73f8d7c427084d0cc5
baser <baserph@yandex.ru>
2023-04-05 23:32:57 +0300

    Правки по style

--- ewm-main/src/main/java/ru/practicum/ewm/events/service/PrivateEventService.java
+++ ewm-main/src/main/java/ru/practicum/ewm/events/service/PrivateEventService.java
@@ -9,6 +9,7 @@ import ru.practicum.ewm.events.dto.EventShortDto;
 import java.util.List;
 
 public interface PrivateEventService {
-    EventFullDto create(Long userId,  CreateEventDto createEventDto);
+    EventFullDto create(Long userId, CreateEventDto createEventDto);
+
     List<EventShortDto> getByCreator(Long userId, Pageable pageable);
 }
