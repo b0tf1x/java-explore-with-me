e6fce1e4cd306869874eeea17f65edd010afaece
baser <baserph@yandex.ru>
2023-04-10 12:20:49 +0300

    Дописал readme

--- ewm-main/src/main/java/ru/practicum/ewm/comments/service/AdminCommentServiceImpl.java
+++ ewm-main/src/main/java/ru/practicum/ewm/comments/service/AdminCommentServiceImpl.java
@@ -3,7 +3,6 @@ package ru.practicum.ewm.comments.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.practicum.ewm.comments.model.Comment;
 import ru.practicum.ewm.comments.repository.CommentsRepository;
 import ru.practicum.ewm.exceptions.NotFoundException;
 
@@ -18,8 +17,8 @@ public class AdminCommentServiceImpl implements AdminCommentService {
         commentsRepository.deleteById(commentId);
     }
 
-    private Comment checkComment(Long commentId) {
-        return commentsRepository.findById(commentId).orElseThrow(() -> {
+    private void checkComment(Long commentId) {
+        commentsRepository.findById(commentId).orElseThrow(() -> {
             throw new NotFoundException("Комментарий не найден");
         });
     }
